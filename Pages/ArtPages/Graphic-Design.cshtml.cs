0a4b39f6f2455301dfe101064d29f7eb9346e2db
KrystalOlive <31352780+KrystalOlive@users.noreply.github.com>
2021-03-01 17:31:56 -0800

    Add files via upload

---
+++ Pages/ArtPages/Graphic-Design.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KrystalWebsite.Pages.ArtPages
+{
+    public class Graphic_DesignModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+    }
+}
